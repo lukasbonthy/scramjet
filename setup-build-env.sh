bf0ce1deee1a15242da481478ec84d282c9729a5
lukasbonthy <126826970+lukasbonthy@users.noreply.github.com>
2025-01-26 13:06:45 -0500

    Update setup-build-env.sh

--- setup-build-env.sh
+++ setup-build-env.sh
@@ -1,15 +1,14 @@
-#!/bin/bash
-
-# Install Rust and cargo
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-source $HOME/.cargo/env
-
-# Install wasm-bindgen
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source "$HOME/.cargo/env" # to import rustup in current shell
+pnpm i
 cargo install wasm-bindgen-cli
-
-# Install wasm-opt (Binaryen)
-curl -L https://github.com/WebAssembly/binaryen/releases/download/version_113/binaryen-version_113-x86_64-linux.tar.gz | tar xz
-sudo mv binaryen-version_113/* /usr/local/bin/
-
-# Install wasm-snip
-cargo install wasm-snip
+VER=$(curl --silent -qI https://github.com/WebAssembly/binaryen/releases/latest | awk -F '/' '/^location/ {print  substr($NF, 1, length($NF)-1)}'); \
+curl -LO https://github.com/WebAssembly/binaryen/releases/download/$VER/binaryen-${VER}-x86_64-linux.tar.gz
+tar xvf binaryen-${VER}-x86_64-linux.tar.gz
+rm -rf binaryen-${VER}-x86_64-linux.tar.gz
+mv binaryen-${VER}/bin/* ~/.local/bin
+mv binaryen-${VER}/lib/* ~/.local/lib
+rm -rf binaryen-${VER}
+cargo install --git https://github.com/r58playz/wasm-snip
+pnpm rewriter:build
+pnpm build
